27027c743fb0294039a897257f7228024bd8afe8
Aditi Dam <dama2468@gmail.com>
2022-11-22 11:29:03 -0500

    testing heroku

--- scripts/api_requests.js
+++ scripts/api_requests.js
@@ -1,27 +1,31 @@
 
 //POST REQUEST FOR ADDING QUESTION
 document.addEventListener("click", function (event) {
+    event.preventDefault();
     if (!event.target.matches("#create_question_button")) return;
     const created_question = {
         name: document.getElementById("question_name"),
-        a: "Aditi",
+        a: document.getElementById("ans1"),
         b: document.getElementById("ans2"),
         c: document.getElementById("ans3"),
         d: document.getElementById("ans4"),
         answer: document.getElementById("answer"),
         };
-    
+  
+        const headers = {'Content-Type':'application/json',
+                    'Access-Control-Allow-Origin':'*',
+                    'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'}
+
         const options = {
         method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-        },
+        headers: headers,
         body: JSON.stringify(created_question),
         };
 
-    fetch('localhost:8080/questions', options)
+    fetch('http://localhost:8080/questions', options)
     .then((data) => renderQuestion(data))
     .then(data => {
+      console.log(data);
        return data.json();
       }).then(update => {
       console.log(update);
@@ -43,7 +47,7 @@ document.addEventListener("click", function (event) {
   
   function renderQuestion(data) {
     const name = document.getElementById("question_name");
-    const a = "Aditi"
+    const a = document.getElementById("ans1");
     const b = document.getElementById("ans2");
     const c = document.getElementById("ans3");
     const d = document.getElementById("ans4");
