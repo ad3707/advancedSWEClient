45332f353a07a0c5ec833b275c3d42a7e2e029a2
Aditi Dam <dama2468@gmail.com>
2022-11-23 00:29:03 -0500

    Added script for api request to update question for client

---
+++ scripts/api_post_user_request.js
@@ -0,0 +1,33 @@
+//POST REQUEST FOR ADDING USER
+document.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (!event.target.matches("#create_user_button")) return;
+    const created_user = {
+        name: document.getElementById("user_name").value,
+        attempted: document.getElementById("attempted").value,
+        correct: document.getElementById("correct").value,
+        };
+  
+        const headers = {'Content-Type':'application/json',
+                    'Access-Control-Allow-Origin':'*',
+                    'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'}
+
+        const options = {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(created_user),
+        };
+
+    fetch('http://localhost:8080/users', options)
+    .then((response) => response.json())
+    .then((data) => renderQuestion(data))
+    .then(data => {
+      console.log(data);
+      })
+  });
+  
+ 
+  
+  
+  
--- scripts/api_update_request.js
+++ scripts/api_update_request.js
@@ -1,10 +1,10 @@
-
-//POST REQUEST FOR ADDING QUESTION
+//POST REQUEST FOR UPDATING QUESTION
 document.addEventListener("click", function (event) {
     event.preventDefault();
 
-    if (!event.target.matches("#create_question_button")) return;
-    const created_question = {
+    if (!event.target.matches("#update_question_button")) return;
+    const updated_question = {
+        id: document.getElementById("question_id").value,
         name: document.getElementById("question_name").value,
         a: document.getElementById("ans1").value,
         b: document.getElementById("ans2").value,
@@ -15,37 +15,21 @@ document.addEventListener("click", function (event) {
   
         const headers = {'Content-Type':'application/json',
                     'Access-Control-Allow-Origin':'*',
-                    'Access-Control-Allow-Methods':'POST,PATCH,OPTIONS'}
+                    'Access-Control-Allow-Methods':'POST,PATCH,PUT,OPTIONS'}
 
         const options = {
         method: 'PUT',
         headers: headers,
-        body: JSON.stringify(created_question),
+        body: JSON.stringify(updated_question),
         };
 
-    fetch('http://localhost:8080/questions', options)
+    var id = document.getElementById("question_id").value; 
+    fetch('http://localhost:8080/questions/' + id, options)
     .then((response) => response.json())
-    .then((data) => renderQuestion(data))
     .then(data => {
       console.log(data);
-      //  return data.json();
       })
   });
-  
-  function renderQuestion(data) {
-    console.log(data);
-    const name = document.getElementById("question_name").value;
-    const a = document.getElementById("ans1").value;
-    const b = document.getElementById("ans2").value;
-    const c = document.getElementById("ans3").value;
-    const d = document.getElementById("ans4").value;
-
-    $("#display_quiz").append("<p>" + name + "</p>");
-    $("#display_quiz").append("<p>" + a + "</p>");
-    $("#display_quiz").append("<p>" + b + "</p>");
-    $("#display_quiz").append("<p>" + c + "</p>");
-    $("#display_quiz").append("<p>" + d + "</p>");
-  }
 
   
   
